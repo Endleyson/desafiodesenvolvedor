5a44e67b40ba610f3f5a8a265ce4edbd07209961
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2020-06-29 15:09:34 -0300

    Update processa_cadastro.php
    inserção da função "now()" para data_alterada

--- processa_cadastro.php
+++ processa_cadastro.php
@@ -8,7 +8,7 @@ $sexo_cadastro= $_POST['sexo_cadastro'];
 
 
 
-$query = "INSERT INTO CADASTROS(nome_cadastro, data_nascimento, sexo_cadastro, data_cadastro) VALUES('$nome_cadastro', '$data_nascimento', '$sexo_cadastro', now())";
+$query = "INSERT INTO CADASTROS(nome_cadastro, data_nascimento, sexo_cadastro, data_cadastro) VALUES('$nome_cadastro', '$data_nascimento', '$sexo_cadastro', now(), now())";
 
 mysqli_query($conexao, $query);
 
